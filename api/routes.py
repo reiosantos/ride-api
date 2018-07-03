59883adf164c5a34ba44bf806cd38847892c9073
reiosantos <ronireiosantos@gmail.com>
2018-07-03 13:52:12 +0300

    Add more ride request routes

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,10 @@
 """
 Urls class , to handel request urls,
 """
+from api.controllers.login_controller import LoginController
+from api.controllers.logout_controller import LogoutController
+from api.controllers.register_controller import RegisterController
+from api.controllers.ride_requests_controller import RideRequestController
 from api.controllers.rides_controller import RidesController
 
 
@@ -17,15 +21,35 @@ class Urls:
         :param: app: takes in the app variable
         :return: urls
         """
+
+        """Ride offers routes"""
         app.add_url_rule('/api/v1/rides/', view_func=RidesController.as_view('get_rides'), methods=['GET'],
                          strict_slashes=False)
-        app.add_url_rule('/api/v1/rides/<int:ride_id>/', view_func=RidesController.as_view('get_one_ride'),
-                         methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/rides/', view_func=RidesController.as_view('post_rides'), methods=["POST"],
+        app.add_url_rule('/api/v1/rides/', view_func=RidesController.as_view('post_ride'), methods=["POST"],
                          strict_slashes=False)
-        app.add_url_rule('/api/v1/rides/<int:ride_id>/requests/', view_func=RidesController.
-                         as_view('request_join_ride'), methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v1/rides/', view_func=RidesController.as_view('update_one_ride'),
+        app.add_url_rule('/api/v1/rides/', view_func=RidesController.as_view('update_a_ride'),
                          methods=['PUT'], strict_slashes=False)
-        app.add_url_rule('/api/v1/rides/<int:ride_id>/', view_func=RidesController.as_view('delete_ride'),
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/', view_func=RidesController.as_view('get_one_ride'),
+                         methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/', view_func=RidesController.as_view('delete_a_ride'),
                          methods=['DELETE'], strict_slashes=False)
+
+        """Routes for ride requests"""
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/requests/', view_func=RideRequestController.
+                         as_view('request_join_ride'), methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/requests/', view_func=RideRequestController.
+                         as_view('view_ride_request'), methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/requests/<string:request_id>/',
+                         view_func=RideRequestController.as_view('delete_ride_request'),
+                         methods=['DELETE'], strict_slashes=False)
+        app.add_url_rule('/api/v1/rides/<string:ride_id>/requests/<string:request_id>/',
+                         view_func=RideRequestController.as_view('update_ride_request'),
+                         methods=['PUT'], strict_slashes=False)
+
+        """authentication routes"""
+        app.add_url_rule('/api/v1/auth/signup/', view_func=RegisterController.as_view('sign_up_user'),
+                         methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v1/auth/login/', view_func=LoginController.as_view('log_in_user'),
+                         methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v1/auth/logout/', view_func=LogoutController.as_view('log_out_user'),
+                         methods=['POST'], strict_slashes=False)
